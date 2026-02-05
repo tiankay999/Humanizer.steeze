09482c15ec7b56cf0e2cadc4123154f347ccd102
tiankay999 <tiankay999@gmail.com>
2026-02-05 13:36:24 +0000

    setted up my server and ready to go

---
+++ backend/index.js
@@ -0,0 +1,37 @@
+const express= require('express');
+const app = express();
+
+
+
+
+
+
+app.get('/health', (req, res) => {
+  res.status(200).json({ message: 'Server is healthy' });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+}   );
