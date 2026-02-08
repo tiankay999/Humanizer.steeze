fff9a2de102bf4530ef76f6dff6e0562f8439d7d
tiankay999 <tiankay999@gmail.com>
2026-02-08 15:18:11 +0000

    remade  the route to solve all bugs

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const User= require("../models/users");
+const User = require("../models/users");
 const bcrypt = require("bcrypt");
 
 
@@ -11,54 +11,48 @@ const bcrypt = require("bcrypt");
 
 
 router.post("/", (req, res) => {
- 
-    try{
-const {name, email, password,phone} = req.body;
 
-if(!name || !email || !password || !phone){
-    return res.status(400).json({message: "All fields are required"});
-}
+    try {
+        const { name, email, password, phone } = req.body;
 
-const existingUser= User.findOne({where: {email}});
+        if (!name || !email || !password || !phone) {
+            return res.status(400).json({ message: "All fields are required" });
+        }
 
-if(existingUser){
-    return res.status(400).json({message: "User already exists"});
-}
- 
-const salt = bcrypt.genSaltSync(10);
-const hash = bcrypt.hashSync(password, salt);
+        const existingUser = User.findOne({ where: { email } });
 
- const newUser=User.create({
-    name,
-    email,
-    password: hash,
-    phone
-})
+        if (existingUser) {
+            return res.status(400).json({ message: "User already exists" });
+        }
 
-if(!newUser){
-    return res.status(400).json({message: "User could not be created"});
-}
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(password, salt);
 
-return res.status(200).json({message: "User created successfully",
-    user: newUser,
-    email: newUser.email,
-    name: newUser.name,
-    phone: newUser.phone,
-    id: newUser.id,
-    
-   
-});
+        const newUser = User.create({
+            name,
+            email,
+            password: hash,
+            phone
+        })
 
-    }catch(error){
-        return res.status(500).json({message: "Internal server error"});
+        if (!newUser) {
+            return res.status(400).json({ message: "User could not be created" });
+        }
+
+        return res.status(201).json({
+            message: "User created successfully",
+            user: newUser,
+            email: newUser.email,
+            name: newUser.name,
+            phone: newUser.phone,
+            id: newUser.id,
+
+
+        });
+
+    } catch (error) {
+        return res.status(500).json({ message: "Internal server error" });
     }
-
-
-
-
-
-
-
 });
 
 
