02143979478443b85ab3b7db6ea55a29bb121177
tiankay999 <tiankay999@gmail.com>
2026-02-09 23:24:07 +0000

    create   router for the llm and also the endpoint

---
+++ backend/routes/LLM.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/users");
+const bcrypt = require("bcrypt");
+
+
+
+router.post("/HumanizerAi",(req,res)=>{
+    try{
+        
+    }
+})
+
+
+
+module.exports = router;    
