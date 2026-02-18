9451446b0a8431f896f768c5bf556622e52c3f6f
tiankay999 <tiankay999@gmail.com>
2026-02-18 21:57:36 +0000

    create  the page for auth (signup and login) 4 another day not today

---
+++ frontend/my-app/app/(auth)/login/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function LoginPage() {
+    return (
+        <div>
+            <h1>Login coming soon</h1>
+        </div>
+    );
+}
---
+++ frontend/my-app/app/(auth)/signup/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function SignupPage() {
+    return (
+        <div>
+            <h1> coming soon</h1>
+        </div>
+    );
+}
