a5ff2be0d8dda61a9f19973b17212ef7af23a5fc
tiankay999 <tiankay999@gmail.com>
2026-04-02 01:12:19 +0000

    google tag

--- frontend/my-app/app/layout.tsx
+++ frontend/my-app/app/layout.tsx
@@ -151,6 +151,21 @@ export default function RootLayout({
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
+        {/* Google tag (gtag.js) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-DKDKJCVS9V"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-DKDKJCVS9V');
+            `,
+          }}
+        />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
